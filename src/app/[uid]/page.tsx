03f1a865a65d66077c5f355de8a3a893ce4d08b9
Oren <oren.menache@gmail.com>
2024-03-21 17:14:17 +0200

    as string

--- src/app/[uid]/page.tsx
+++ src/app/[uid]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default async function Page({ params }: { params: { uid: string } }) {
     if (!('uid' in params))
         return <h1>No uid, shouldn't be possible. Check with admin</h1>;
-    const uid = params.uid as string;
+    const uid = params.uid;
     return (
         <div>
             <pre>{uid}</pre>
