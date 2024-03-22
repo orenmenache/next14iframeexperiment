1a8eec19910da057fc710f7baef0d334d6269e4c
Oren <oren.menache@gmail.com>
2024-03-22 13:15:37 +0200

    style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
     return (
         <main>
-            <iframe src="https://clownfish-app-atszh.ondigitalocean.app/another" />
+            <iframe
+                src="https://clownfish-app-atszh.ondigitalocean.app/another"
+                style={{
+                    width: '100%',
+                    height: '100vh',
+                    border: 'none',
+                    backgroundColor: 'gray',
+                }}
+            />
         </main>
     );
 }
