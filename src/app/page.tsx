e312d430d3230980d74f26addc94b83d7e70a801
Ram Gerassy <ram.gerassy@gmail.com>
2024-04-14 15:38:27 +0300

    updated mock array type and keys to match the db data returned

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import getClientData from "@/server/Functions/getClientData";
 import getClientVideos from "@/server/Functions/getClientVideos";
 import getEpisodesData from "@/server/Functions/getEpisodeData";
 import organizeEpisodesData, {
+   MinifiedChapters,
    MinifiedEpisodes,
 } from "@/server/Functions/organizeEpisodesData";
 import { Client, DyntubeVideo } from "@/server/types/dbSchemes";
@@ -25,112 +26,82 @@ export default async function Home() {
    const clientName = "Shmual";
    const clientId = "345";
    const url = `https://clownfish-app-atszh.ondigitalocean.app/protected?clientId=${clientId}&clientName=${clientName}`;
-   const dummy: Academia[] = [
+   const dummy: MinifiedChapters[] = [
       {
          episodes: [
             {
-               name: "1. Forex Double Top And Double Bottom Formation Patterns",
+               episode:
+                  "1. Forex Double Top And Double Bottom Formation Patterns",
                key: "sivLZRsA7UejUo4gRZN1EQ",
             },
             {
-               name: "2. Learn Forex Head And Shoulders Pattern",
+               episode: "2. Learn Forex Head And Shoulders Pattern",
                key: "Jth83q6cfUCO7zLYIVkrxw",
             },
             {
-               name: "3. Forex Inverse Head And Shoulders Pattern",
+               episode: "3. Forex Inverse Head And Shoulders Pattern",
                key: "CUo2wHYlUu8DFsuxsHdA",
             },
             {
-               name: "4. Forex Bull Flag Formation Patterns",
+               episode: "4. Forex Bull Flag Formation Patterns",
                key: "U7J0rcQsSkuxqcQsoRTaqg",
             },
             {
-               name: "5. Forex Bear Flag Patterns",
+               episode: "5. Forex Bear Flag Patterns",
                key: "5kkIIaNiNkmblMFk4QPiw",
             },
             {
-               name: "6. Forex Bullish And Bearish Pennant Formation",
+               episode: "6. Forex Bullish And Bearish Pennant Formation",
                key: "YbIO6ZKx20m6Hu4P8897JA",
             },
             {
-               name: "7. Forex Falling Wedge Pattern",
+               episode: "7. Forex Falling Wedge Pattern",
                key: "DFE6UGTFkkCbZTBvmAqZbg",
             },
             {
-               name: "8. Forex Ascending And Descending Triangle Formations",
+               episode: "8. Forex Ascending And Descending Triangle Formations",
                key: "3DN70JImUqjiAI9ZEcg",
             },
             {
-               name: "9. Forex Symmetrical Triangle Pattern",
+               episode: "9. Forex Symmetrical Triangle Pattern",
                key: "HOnbqTLDzECraLHSIm9qw",
             },
          ],
          chapter: "Chart Formation Patterns",
+         levelId: 1,
       },
       {
          chapter: "Forex Indicators",
+         levelId: 1,
          episodes: [
             {
-               name: "10. Forex Double Top And Double Bottom Formation Patterns",
+               episode:
+                  "10. Forex Double Top And Double Bottom Formation Patterns",
                key: "R0sEV05rEIkRaBfV1zw",
             },
             {
-               name: "20. Learn Forex Head And Shoulders Pattern",
+               episode: "20. Learn Forex Head And Shoulders Pattern",
                key: "YbrGhfGET0maoLe0MIaUQ",
             },
             {
-               name: "30. Forex Inverse Head And Shoulders Pattern",
+               episode: "30. Forex Inverse Head And Shoulders Pattern",
                key: "0UF3YCY0UOhdktn4v3sxQ",
             },
             {
-               name: "40. Forex Bull Flag Formation Patterns",
+               episode: "40. Forex Bull Flag Formation Patterns",
                key: "v1nTG6qMxUWkCDAMtkwpmA",
             },
             {
-               name: "50. Forex Bear Flag Patterns",
+               episode: "50. Forex Bear Flag Patterns",
                key: "QdCM4mdkGkSeAaOxiL02Jg",
             },
          ],
       },
    ];
-   const clientRawData: any = await getClientData("Traders Lab");
-   const clientData: Client = await clientRawData[0][0];
-   console.log(clientData);
-   const clientVideos: DyntubeVideo[] = (
-      await getClientVideos(clientData.dyntube_project_id)
-   )[0] as DyntubeVideo[];
-   //console.log(clientVideos);
-   const episodes: MinifiedEpisodes[] = clientVideos.map((video) => {
-      let regex = /^\d+\.\s/;
-      let title = video.title.replace(regex, "");
-      return { episode: title, key: video.channelKey };
-   });
-   const dyntubeEpisodes = Array.from(episodes);
-   let episodesData: any = [];
-   while (episodes.length > 0) {
-      const tempEpisodesData = (
-         await getEpisodesData(
-            episodes.splice(0, 10).map((episode) => episode.episode),
-            clientData.lang
-         )
-      )[0] as any[];
-      episodesData = [...episodesData, ...tempEpisodesData];
-   }
-   console.log(dyntubeEpisodes);
-   const data = organizeEpisodesData(episodesData, dyntubeEpisodes);
 
-   fs.writeFile("data.json", JSON.stringify(data), (err) => {
-      if (err) {
-         console.error("Error writing data to file:", err);
-      } else {
-         console.log("Data written to file successfully!");
-      }
-   });
-
-   console.log(data);
    return (
       <main>
-         <AcademyApp videoData={data} />
+         <AcademyApp videoData={dummy} />
       </main>
    );
 }
--- src/app/protected/[...slug]/page.tsx
+++ src/app/protected/[...slug]/page.tsx
@@ -1,5 +1,8 @@
+import { Academia } from "@/app/page";
 import { MYSQL_DB } from "@/server/classes/MYSQL_DB";
+import AcademyApp from "@/server/Components/AcademyApp";
 import FullAcademy from "@/server/Components/FullAcademy";
+import { MinifiedChapters } from "@/server/Functions/organizeEpisodesData";
 import { runFunctionWithRetry } from "@/server/Functions/RunFunctionWithRetry";
 import axios from "axios";
 import { headers } from "next/headers";
@@ -16,6 +19,77 @@ type PageProps = {
 };
 
 // test
+const dummy: MinifiedChapters[] = [
+   {
+      episodes: [
+         {
+            episode: "1. Forex Double Top And Double Bottom Formation Patterns",
+            key: "sivLZRsA7UejUo4gRZN1EQ",
+         },
+         {
+            episode: "2. Learn Forex Head And Shoulders Pattern",
+            key: "Jth83q6cfUCO7zLYIVkrxw",
+         },
+         {
+            episode: "3. Forex Inverse Head And Shoulders Pattern",
+            key: "CUo2wHYlUu8DFsuxsHdA",
+         },
+         {
+            episode: "4. Forex Bull Flag Formation Patterns",
+            key: "U7J0rcQsSkuxqcQsoRTaqg",
+         },
+         {
+            episode: "5. Forex Bear Flag Patterns",
+            key: "5kkIIaNiNkmblMFk4QPiw",
+         },
+         {
+            episode: "6. Forex Bullish And Bearish Pennant Formation",
+            key: "YbIO6ZKx20m6Hu4P8897JA",
+         },
+         {
+            episode: "7. Forex Falling Wedge Pattern",
+            key: "DFE6UGTFkkCbZTBvmAqZbg",
+         },
+         {
+            episode: "8. Forex Ascending And Descending Triangle Formations",
+            key: "3DN70JImUqjiAI9ZEcg",
+         },
+         {
+            episode: "9. Forex Symmetrical Triangle Pattern",
+            key: "HOnbqTLDzECraLHSIm9qw",
+         },
+      ],
+      chapter: "Chart Formation Patterns",
+      levelId: 1,
+   },
+   {
+      chapter: "Forex Indicators",
+      levelId: 1,
+      episodes: [
+         {
+            episode:
+               "10. Forex Double Top And Double Bottom Formation Patterns",
+            key: "R0sEV05rEIkRaBfV1zw",
+         },
+         {
+            episode: "20. Learn Forex Head And Shoulders Pattern",
+            key: "YbrGhfGET0maoLe0MIaUQ",
+         },
+         {
+            episode: "30. Forex Inverse Head And Shoulders Pattern",
+            key: "0UF3YCY0UOhdktn4v3sxQ",
+         },
+         {
+            episode: "40. Forex Bull Flag Formation Patterns",
+            key: "v1nTG6qMxUWkCDAMtkwpmA",
+         },
+         {
+            episode: "50. Forex Bear Flag Patterns",
+            key: "QdCM4mdkGkSeAaOxiL02Jg",
+         },
+      ],
+   },
+];
 
 export default async function Page({ params }: PageProps) {
    console.log("You got to search");
@@ -32,11 +106,7 @@ export default async function Page({ params }: PageProps) {
    console.log(clientVideos);
    return (
       <div style={{ height: "500px", width: "500px" }}>
-         <FullAcademy
-            params={{
-               videos: clientVideos,
-            }}
-         />
+         <AcademyApp videoData={dummy} />
       </div>
    );
 }
